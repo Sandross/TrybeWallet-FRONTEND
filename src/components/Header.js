8ede6bb919e6c8fae33fe85906b62f377a4909e8
Sandro Bistene <sandrobistene@Sandros-MacBook-Pro.local>
2022-04-04 17:44:48 -0300

    req 8 ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { savedExpenses } = this.props;
+    console.log(savedExpenses);
     return (
       <table>
         <tr>
@@ -15,9 +19,43 @@ class Header extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {
+          savedExpenses.map(
+            ({ description, tag, method, currency, value, exchangeRates }, i) => (
+              <tr key={ `${tag}_${value}` }>
+                <td key={ `${description}_${i}` }>{description}</td>
+                <td key={ `${tag}_${i}` }>{tag}</td>
+                <td key={ `${method}_${i}` }>{method}</td>
+                <td key={ `${value}_${i}` }>{(+value).toFixed(2)}</td>
+                {currency === 'USD' && <td key={ i }>Dólar Comercial</td>}
+                {currency === 'EUR' && <td key={ i }>Euro</td>}
+                { currency === !'EUR'
+            && currency === !'USD'
+            && <td key={ i }>{`${currency}/Real Brasileiro`}</td>}
+                <td key={ `${exchangeRates[currency].ask}` }>
+                  {(+exchangeRates[currency].ask).toFixed(2)}
+                </td>
+                <td key={ `${value * exchangeRates[currency].ask}` }>
+                  {' '}
+                  {(+value * +exchangeRates[currency].ask).toFixed(2)}
+                  {' '}
+                </td>
+                <td key={ `${method}Real` }>Real</td>
+              </tr>
+            ),
+          )
+        }
       </table>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  savedExpenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  savedExpenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
